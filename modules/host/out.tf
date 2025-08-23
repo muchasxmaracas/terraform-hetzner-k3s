95af577883befb779b784dc32fd55e2006631583
Sandro Marton <sandro.marton@lgt.com>
2025-08-23 21:54:39 +0200

    chore: fix

--- modules/host/out.tf
+++ modules/host/out.tf
@@ -27,3 +27,13 @@ output "domain_assignments" {
     }
   ]
 }
+
+output "control_planes_public_ipv6" {
+  value       = hcloud_server.control_plane[*].ipv6_address
+  description = "List of public IPv6 addresses of the control plane nodes."
+}
+
+output "agents_public_ipv6" {
+  value       = hcloud_server.agent[*].ipv6_address
+  description = "List of public IPv6 addresses of the agent nodes."
+}
