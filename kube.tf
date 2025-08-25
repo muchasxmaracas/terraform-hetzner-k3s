20d593df1cf9da8eb707aa96389a16de9f6b383a
muchasxmaracas <sandromarton95@gmail.com>
2025-08-25 23:54:10 +0200

    chore: rollback kube-hetzner

--- kube.tf
+++ kube.tf
@@ -23,7 +23,7 @@ module "kube-hetzner" {
   source = "kube-hetzner/kube-hetzner/hcloud"
   #    When using the terraform registry as source, you can optionally specify a version number.
   #    See https://registry.terraform.io/modules/kube-hetzner/kube-hetzner/hcloud for the available versions
-  version = "2.18.1"
+  version = "2.15.3"
   # 2. For local dev, path to the git repo
   # source = "../../kube-hetzner/"
   # 3. If you want to use the latest master branch (see https://developer.hashicorp.com/terraform/language/modules/sources#github), use
